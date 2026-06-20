700ce31ee1a3ad8e689c889a5bc4bb874a8024d5
G-HARSHITH <harshith.22bce20049@vitapstudent.ac.in>
2026-06-20 14:40:21 +0530

    made eduction page

--- src/components/QuantumCodon.tsx
+++ src/components/QuantumCodon.tsx
@@ -894,8 +894,8 @@ const footerNavigation = {
           id: "education",
           name: "Education",
           items: [
-            { name: "Academic Training", href: "/education#academic-training" },
-            { name: "Certifications", href: "/education" },
+            { name: "Academic Training", href: "/services/education#academic-training" },
+            { name: "Certifications", href: "/services/education" },
           ],
         },
         {
